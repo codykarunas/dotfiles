5567c736755def1b16a023ab03525b088afb1cb2
Cody Karunas <codykarunas@gmail.com>
2019-02-05 12:09:32 -0500

    changed home path

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo `ln -s /Users/codykarunas/.dotfiles/.bashrc ~/`
-echo `ln -s /Users/codykarunas/.dotfiles/.gitconfig ~/`
-echo `ln -s /Users/codykarunas/.dotfiles/.hushlogin ~/`
-echo `ln -s /Users/codykarunas/.dotfiles/.railsrc ~/`
-echo `ln -s /Users/codykarunas/.dotfiles/.tmux.conf ~/`
-echo `ln -s /Users/codykarunas/.dotfiles/.vimrc ~/`
-echo `ln -s /Users/codykarunas/.dotfiles/.ycm_extra_conf.py ~/`
-echo `ln -s /Users/codykarunas/.dotfiles/.zprofile ~/`
-echo `ln -s /Users/codykarunas/.dotfiles/.zshrc ~/`
+echo `ln -s ~/.dotfiles/.bashrc ~/`
+echo `ln -s ~/.dotfiles/.gitconfig ~/`
+echo `ln -s ~/.dotfiles/.hushlogin ~/`
+echo `ln -s ~/.dotfiles/.railsrc ~/`
+echo `ln -s ~/.dotfiles/.tmux.conf ~/`
+echo `ln -s ~/.dotfiles/.vimrc ~/`
+echo `ln -s ~/.dotfiles/.ycm_extra_conf.py ~/`
+echo `ln -s ~/.dotfiles/.zprofile ~/`
+echo `ln -s ~/.dotfiles/.zshrc ~/`
